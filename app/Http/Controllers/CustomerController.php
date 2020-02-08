7031f20d6061f5b6282d244cd81736ae8d63f52c
root <root@localhost.localdomain>
2020-02-08 19:23:36 +0800

    .

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -7,9 +7,11 @@ use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
 use Route;
 use View;
+use App\Customer;
 class CustomerController extends Controller
 {
     public function index(){
-        return View::make('board');
+       $customers = Customer::all();
+       return View::make('board',['customers' => $customers]);
     }
 }
