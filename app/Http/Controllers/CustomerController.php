10c161198a75f51143741a2b3c86a78e596eb414
Hong-Kai-Jyun <s99101219@stu.edu.tw>
2020-02-09 16:07:51 +0800

    .

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -18,7 +18,7 @@ class CustomerController extends Controller
     public function query(){
         echo "id:".$_POST['id']."</br>";
        $id = $_POST['id'];
-       $customers = DB::select("select * from customers where id =='{$id}' ");
+       $customers = DB::select("select * from customers where id ='{$id}' ");
        echo "select * from customers where '{$id}' "; 
        return View::make('updata',['customers' => $customers]);
     } 
