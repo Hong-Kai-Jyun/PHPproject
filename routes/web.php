298d41a5a975551270b9af8735f0ee5495da13fb
Hong-Kai-Jyun <s99101219@stu.edu.tw>
2020-02-09 21:02:44 +0800

    .

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,10 @@ Route::get('hello','Hello\DemoController@index');
 Route::resource('customer','CustomerController');
 
 Route::resource('cars','CarsController');
-Route::post('add_coustomers','CustomerController@add_coustomers');
+Route::get('add_coustomers', function () {
+    return view('add_coustomers_index');
+});
+//Route::post('add_coustomers','CustomerController@add_coustomers');
 Route::post('updata','CustomerController@query');
 Route::post('updata_2','CustomerController@updata');
 
